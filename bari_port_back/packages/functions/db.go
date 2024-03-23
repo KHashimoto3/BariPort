bc8fc04ccf58c30402f7573b3c8072a873571514
Takahashi Shota <shohh6119@gmial.com>
2024-03-23 17:18:34 +0900

    Impl messages list

--- bari_port_back/packages/functions/db.go
+++ bari_port_back/packages/functions/db.go
@@ -9,29 +9,56 @@ import (
 	"github.com/guregu/dynamo"
 )
 
-//dynamo db接続関数
+// dynamo db接続関数
 func connect(dbName string) dynamo.Table {
 	sess := session.Must(session.NewSession())
 	db := dynamo.New(sess, &aws.Config{Region: aws.String("ap-northeast-1")})
 	return db.Table(dbName)
 }
 
-//各テーブルのデータ構造
-//companiesテーブル
+// 各テーブルのデータ構造
+// companiesテーブル
 type Company struct {
-	Id 		string `dynamo:"id"`
-	Name 	string `dynamo:"name"`
+	Id   string `dynamo:"id"`
+	Name string `dynamo:"name"`
 }
 
-//projectsテーブル
+// projectsテーブル
 type Project struct {
-	Id 			string `dynamo:"id"`
-	CompanyId 	string `dynamo:"companyId"`
-	Name 		string `dynamo:"name"`
+	Id          string `dynamo:"id"`
+	CompanyId   string `dynamo:"companyId"`
+	Name        string `dynamo:"name"`
 	Description string `dynamo:"description"`
 }
 
-//companyのデータを取得
+// messagesテーブル
+type Message struct {
+	Id         string `dynamo:"id"`
+	UserId     string `dynamo:"userId"`
+	CompanyId  string `dynamo:"companyId"`
+	ChatRoomId string `dynamo:"chatRoomId"`
+	Text       string `dynamo:"text"`
+	ImgUrl     string `dynamo:"imgUrl"`
+	IsMine     bool   `dynamo:"isMine"`
+	SendAt     string `dynamo:"sendAt"`
+}
+
+// chatRoomsテーブル
+type ChatRoom struct {
+	Id        string `dynamo:"id"`
+	Name      string `dynamo:"displayName"`
+	Type      string `dynamo:"apiKey"`
+	ProjectId string `dynamo:"projectId"`
+}
+
+// usersテーブル
+type User struct {
+	Id          string `dynamo:"id"`
+	DisplayName string `dynamo:"displayName"`
+	ApiKey      string `dynamo:"apiKey"`
+}
+
+// companyのデータを取得
 func GetCompany(companyId string) (Company, error) {
 	table := connect(os.Getenv("SST_Table_tableName_companies"))
 	var company Company
@@ -58,3 +85,54 @@ func GetProjects() ([]Project, error) {
 	return projects, nil
 }
 
+func GetMessages() ([]Message, error) {
+	table := connect(os.Getenv("SST_Table_tableName_messages"))
+	var messages []Message
+	err := table.Scan().All(&messages)
+	if err != nil {
+		if errors.Is(err, dynamo.ErrNotFound) {
+			return messages, errors.New("messages not found")
+		}
+		return messages, err
+	}
+	return messages, nil
+}
+
+func GetChatRooms() ([]ChatRoom, error) {
+	table := connect(os.Getenv("SST_Table_tableName_users"))
+	var chatRooms []ChatRoom
+	err := table.Scan().All(&chatRooms)
+	if err != nil {
+		if errors.Is(err, dynamo.ErrNotFound) {
+			return chatRooms, errors.New("chatRooms not found")
+		}
+		return chatRooms, err
+	}
+	return chatRooms, nil
+}
+
+func GetUsers() ([]User, error) {
+	table := connect(os.Getenv("SST_Table_tableName_users"))
+	var users []User
+	err := table.Scan().All(&users)
+	if err != nil {
+		if errors.Is(err, dynamo.ErrNotFound) {
+			return users, errors.New("users not found")
+		}
+		return users, err
+	}
+	return users, nil
+}
+
+func GetUser(userId string) (User, error) {
+	table := connect(os.Getenv("SST_Table_tableName_users"))
+	var user User
+	err := table.Get("id", userId).One(&user)
+	if err != nil {
+		if errors.Is(err, dynamo.ErrNotFound) {
+			return user, errors.New("user not found")
+		}
+		return user, err
+	}
+	return user, nil
+}
--- bari_port_back/packages/functions/handlers.go
+++ bari_port_back/packages/functions/handlers.go
@@ -12,15 +12,38 @@ type HelloResponse struct {
 }
 
 type GetProjectCompany struct {
-	Id		string `json:"id"`
-	Name 	string `json:"name"`
+	Id   string `json:"id"`
+	Name string `json:"name"`
 }
 
 type GetProjectsResponse struct {
-	Id		string `json:"id"`
-	Name 	string `json:"name"`
-	Description string `json:"description"`
-	Company GetProjectCompany `json:"company"`
+	Id          string            `json:"id"`
+	Name        string            `json:"name"`
+	Description string            `json:"description"`
+	Company     GetProjectCompany `json:"company"`
+}
+
+type GetMessageChatRoom struct {
+	Id     string `dynamo:"id"`
+	Name   string `dynamo:"text"`
+	ImgUrl string `dynamo:"imgUrl"`
+	IsMine bool   `dynamo:"isMine"`
+	SendAt string `dynamo:"sendAt"`
+}
+
+type GetMessageUser struct {
+	Id          string `dynamo:"id"`
+	DisplayName string `dynamo:"displayName"`
+	ApiKey      string `dynamo:"apiKey"`
+}
+
+type GetMessagesResponse struct {
+	Id       string             `dynamo:"id"`
+	User     GetMessageUser     `dynamo:"userId"`
+	ChatRoom GetMessageChatRoom `dynamo:"chatRoomId"`
+	Text     string             `dynamo:"text"`
+	ImgUrl   string             `dynamo:"imgUrl"`
+	SendAt   string             `dynamo:"sendAt"`
 }
 
 func HandlerHello(ctx context.Context) (events.APIGatewayProxyResponse, error) {
@@ -33,7 +56,7 @@ func HandlerHello(ctx context.Context) (events.APIGatewayProxyResponse, error) {
 	}, nil
 }
 
-//プロジェクトの一覧を、企業情報と共に返すLambdaハンドラ
+// プロジェクトの一覧を、企業情報と共に返すLambdaハンドラ
 func HandlerGetProjects(ctx context.Context) (events.APIGatewayProxyResponse, error) {
 	projects, err := GetProjects()
 
@@ -50,15 +73,74 @@ func HandlerGetProjects(ctx context.Context) (events.APIGatewayProxyResponse, er
 		}
 
 		company := GetProjectCompany{
-			Id: companyRes.Id,
+			Id:   companyRes.Id,
 			Name: companyRes.Name,
 		}
 
 		res = append(res, GetProjectsResponse{
-			Id: project.Id,
-			Name: project.Name,
+			Id:          project.Id,
+			Name:        project.Name,
 			Description: project.Description,
-			Company: company,
+			Company:     company,
+		})
+	}
+
+	body, _ := json.Marshal(res)
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(body),
+		StatusCode: 200,
+	}, nil
+}
+
+// メッセージ一覧を返すLambdaハンドラ
+func HandlerGetMessages(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	chatRoomId := request.QueryStringParameters["chatRoomId"]
+
+	allMessages, err := GetMessages()
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	var messages []Message
+	for _, msg := range allMessages {
+		if msg.ChatRoomId == chatRoomId {
+			messages = append(messages, msg)
+		}
+	}
+
+	// チャットルームを取得
+	chatRoomRes, err := GetChatRooms(chatRoomId)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	chatRoom := GetMessageChatRoom{
+		Id:   chatRoomRes.Id,
+		Name: chatRoomRes.Name,
+	}
+
+	var res []GetMessagesResponse
+	for _, message := range messages {
+
+		// メッセージに紐づくユーザーを取得
+		userRes, err := GetUser(message.UserId)
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
+		user := GetMessageUser{
+			Id:          userRes.Id,
+			DisplayName: userRes.DisplayName,
+			ApiKey:      userRes.ApiKey,
+		}
+
+		res = append(res, GetMessagesResponse{
+			Id:       message.Id,
+			User:     user,
+			ChatRoom: chatRoom,
+			Text:     message.Text,
+			ImgUrl:   message.ImgUrl,
+			SendAt:   message.SendAt,
 		})
 	}
 
